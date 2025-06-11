6118be4535032ac5071fd1d844a3b518fc7bf20a
Isaac kariithi <isaacjayden27@gmail.com>
2025-06-11 18:29:41 +0300

    Added model

--- app/src/main/java/com/project/leaflens/utils/TFLiteModelLoader.kt
+++ app/src/main/java/com/project/leaflens/utils/TFLiteModelLoader.kt
@@ -7,7 +7,7 @@ import java.nio.channels.FileChannel
 
 object TFLiteModelLoader {
     fun loadModelFile(application: Application): MappedByteBuffer {
-        val fileDescriptor = application.assets.openFd("model.tflite")
+        val fileDescriptor = application.assets.openFd("mobilenetv2_v1_44_0.996.tflite")
         val inputStream = FileInputStream(fileDescriptor.fileDescriptor)
         val fileChannel = inputStream.channel
         return fileChannel.map(FileChannel.MapMode.READ_ONLY, fileDescriptor.startOffset, fileDescriptor.declaredLength)
